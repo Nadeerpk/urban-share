f2ab49667ad70fe2fb78517440a010a4e0ec052f
Nadeerpk <Nadeerpk11@gmail.com>
2025-07-21 19:00:55 +0530

    Added reviews app

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Review(models.Model):
+    """
+    Model representing a review for a listing.
+    """
+    listing = models.ForeignKey('listings.Listing', on_delete=models.CASCADE)
+    user = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
+    rating = models.IntegerField(validators=[MinValueValidator(1),
+                                             MaxValueValidator(5)])
+    comment = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Review by {self.user.username} for {self.listing.title}'
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Review
+from .forms import ReviewForm
 
 # Create your views here.
+
+@login_required
+def edit_review(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    if review.user != request.user:
+        return redirect('listing_detail', pk=review.listing.pk)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            return redirect('listing_detail', pk=review.listing.pk)
+    else:
+        form = ReviewForm(instance=review)
+    return render(request, 'reviews/edit_review.html', {'form': form, 'review': review})
+
+@login_required
+def delete_review(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    if review.user == request.user and request.method == 'POST':
+        listing_pk = review.listing.pk
+        review.delete()
+        return redirect('listing_detail', pk=listing_pk)
+    return redirect('listing_detail', pk=review.listing.pk)
